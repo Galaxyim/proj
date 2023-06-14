5392719130060086558fd0b3367e6de1871b2bf9
macaco <murilopresentes09@gmail.com>
2023-06-14 15:31:50 -0300

    Changes

--- script.js
+++ script.js
@@ -7,13 +7,10 @@ function togglemode() {
 
   if (html.classList.contains('light')) {
     img.setAttribute('src', './assets/avatar-light.png')
-  
   }
 
   else {
     img.setAttribute('src', './assets/avatar-dark.png')
-
-
   }
 
 
